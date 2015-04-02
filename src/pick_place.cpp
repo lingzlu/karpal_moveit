202c65aa9cf9fdb871d3a53f1a73acff5bf84f9b
Lingzhou Lu <lingzhoulu@Joes-MacBook-Pro.local>
2015-04-02 13:11:35 -0500

    changes

--- src/pick_place.cpp
+++ src/pick_place.cpp
@@ -7,37 +7,84 @@ void pick(moveit::planning_interface::MoveGroup &group)
 {
   std::vector<moveit_msgs::Grasp> possible_grasps;
 
-    geometry_msgs::PoseStamped p = group.getRandomPose();
-    p.pose.orientation.x = 0;
-    p.pose.orientation.y = 0;
-    p.pose.orientation.z = 0;
-    p.pose.orientation.w = 1;
-
-    moveit_msgs::Grasp g;
-    g.grasp_pose = p;
-    g.pre_grasp_approach.direction.vector.x = 1.0;
-    g.pre_grasp_approach.min_distance = 0.2;
-    g.pre_grasp_approach.desired_distance = 0.4;
-
-    g.post_grasp_retreat.direction.vector.z = 1.0;
-    g.post_grasp_retreat.direction.header = p.header;
-    g.post_grasp_retreat.min_distance = 0.1;
-    g.post_grasp_retreat.desired_distance = 0.27;
+    moveit_msgs::Grasp grasp;
+    grasp.id = "Test Grasp";
+    grasp.grasp_quality = 1.0;
 
     std::vector<std::string> joint_names;
     joint_names.push_back("right_gripper");
 
-    g.pre_grasp_posture.joint_names = joint_names;
-    g.pre_grasp_posture.points.resize(1);
-    g.pre_grasp_posture.points[0].positions.resize(1);
-    g.pre_grasp_posture.points[0].positions[0] = 1;
+    trajectory_msgs::JointTrajectory pre_grasp_posture;
+    pre_grasp_posture.header.frame_id = "root";
+    pre_grasp_posture.header.stamp = ros::Time::now();
+    pre_grasp_posture.joint_names = joint_names;
+    pre_grasp_posture.points.resize(1);
+    pre_grasp_posture.points[0].positions.push_back(0);
+    pre_grasp_posture.points[0].positions.push_back(0);
+    pre_grasp_posture.points[0].positions.push_back(0);
+    pre_grasp_posture.points[0].positions.push_back(0);
+    pre_grasp_posture.points[0].positions.push_back(0);
+    pre_grasp_posture.points[0].positions.push_back(0);
+    pre_grasp_posture.points[0].time_from_start = ros::Duration(4.0);
+    grasp.pre_grasp_posture = pre_grasp_posture;
 
-    g.grasp_posture.joint_names = joint_names;
-    g.grasp_posture.points.resize(1);
-    g.grasp_posture.points[0].positions.resize(1);
-    g.grasp_posture.points[0].positions[0] = 0;
+    // grasp.pre_grasp_posture.joint_names = joint_names;
+    // grasp.pre_grasp_posture.points.resize(1);
+    // grasp.pre_grasp_posture.points[0].positions.resize(1);
+    // grasp.pre_grasp_posture.points[0].positions[0] = 1;
 
-    possible_grasps.push_back(g);
+    trajectory_msgs::JointTrajectory grasp_posture;
+    grasp_posture.header.frame_id = "root";
+    grasp_posture.header.stamp = ros::Time::now();
+    grasp_posture.joint_names = joint_names;
+    grasp_posture.points.resize(1);
+    grasp_posture.points[0].positions.push_back(0.15);
+    grasp_posture.points[0].positions.push_back(0.15);
+    grasp_posture.points[0].positions.push_back(0.15);
+    grasp_posture.points[0].positions.push_back(0.6981);
+    grasp_posture.points[0].positions.push_back(0.6981);
+    grasp_posture.points[0].positions.push_back(0.6981);
+    grasp_posture.points[0].time_from_start = ros::Duration(4.0);
+    grasp.grasp_posture = grasp_posture;
+
+    // geometry_msgs::PoseStamped p = group.getRandomPose();
+    // p.pose.orientation.x = 0;
+    // p.pose.orientation.y = 0;
+    // p.pose.orientation.z = 0;
+    // p.pose.orientation.w = 1;
+    geometry_msgs::PoseStamped grasp_pose_msg;
+    grasp_pose_msg.header.stamp = ros::Time::now();
+    grasp_pose_msg.header.frame_id = "/root";
+    grasp_pose_msg.pose = object_pose;
+
+    grasp_pose_msg.pose.position.y -= 0.1525;
+
+    Eigen::AngleAxisd rollAngle(0, Eigen::Vector3d::UnitZ());
+    Eigen::AngleAxisd yawAngle(0, Eigen::Vector3d::UnitY());
+    Eigen::AngleAxisd pitchAngle(M_PI/2, Eigen::Vector3d::UnitX());
+    Eigen::Quaternion<double> quat = rollAngle * yawAngle * pitchAngle;
+    grasp_pose_msg.pose.orientation.x = quat.x();
+    grasp_pose_msg.pose.orientation.y = quat.y();
+    grasp_pose_msg.pose.orientation.z = quat.z();
+    grasp_pose_msg.pose.orientation.w = quat.w();
+    grasp.grasp_pose = grasp_pose_msg;
+
+    // grasp.grasp_pose = p;
+    grasp.pre_grasp_approach.direction.vector.x = 1.0;
+    grasp.pre_grasp_approach.min_distance = 0.2;
+    grasp.pre_grasp_approach.desired_distance = 0.4;
+
+    grasp.post_grasp_retreat.direction.vector.z = 1.0;
+    grasp.post_grasp_retreat.direction.header = p.header;
+    grasp.post_grasp_retreat.min_distance = 0.1;
+    grasp.post_grasp_retreat.desired_distance = 0.27;
+
+    // grasp.grasp_posture.joint_names = joint_names;
+    // grasp.grasp_posture.points.resize(1);
+    // grasp.grasp_posture.points[0].positions.resize(1);
+    // grasp.grasp_posture.points[0].positions[0] = 0;
+
+    possible_grasps.push_back(grasp);
     group.pick("block", possible_grasps);
 }
 
@@ -51,22 +98,22 @@ void place(moveit::planning_interface::MoveGroup &group)
     p.pose.orientation.y = 0;
     p.pose.orientation.z = 0;
     p.pose.orientation.w = 1;
-    moveit_msgs::PlaceLocation g;
-    g.place_pose = p;
-    g.pre_place_approach.direction.vector.x = 1.0;
-    g.post_place_retreat.direction.vector.z = 1.0;
-    g.post_place_retreat.direction.header = p.header;
-    g.pre_place_approach.min_distance = 0.2;
-    g.pre_place_approach.desired_distance = 0.4;
-    g.post_place_retreat.min_distance = 0.1;
-    g.post_place_retreat.desired_distance = 0.27;
+    moveit_msgs::PlaceLocation grasp;
+    grasp.place_pose = p;
+    grasp.pre_place_approach.direction.vector.x = 1.0;
+    grasp.post_place_retreat.direction.vector.z = 1.0;
+    grasp.post_place_retreat.direction.header = p.header;
+    grasp.pre_place_approach.min_distance = 0.2;
+    grasp.pre_place_approach.desired_distance = 0.4;
+    grasp.post_place_retreat.min_distance = 0.1;
+    grasp.post_place_retreat.desired_distance = 0.27;
 
-    g.post_place_posture.joint_names.resize(1, "right_gripper");
-    g.post_place_posture.points.resize(1);
-    g.post_place_posture.points[0].positions.resize(1);
-    g.post_place_posture.points[0].positions[0] = 0;
+    grasp.post_place_posture.joint_names.resize(1, "right_gripper");
+    grasp.post_place_posture.points.resize(1);
+    grasp.post_place_posture.points[0].positions.resize(1);
+    grasp.post_place_posture.points[0].positions[0] = 0;
 
-    loc.push_back(g);
+    loc.push_back(grasp);
   }
   group.place("block", loc);
 }
