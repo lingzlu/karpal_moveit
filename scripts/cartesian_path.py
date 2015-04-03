af904ca56bdb72e6864deaa7518c1274a193883f
Lingzhou Lu <lu3421@atlas.cs.ou.edu>
2015-04-03 17:59:18 -0500

    final version python cartesian path

--- scripts/cartesian_path.py
+++ scripts/cartesian_path.py
@@ -9,7 +9,8 @@ import moveit_msgs.msg
 from copy import deepcopy
 
 class Path_Planning:
-	def __init__(self, arm="left_arm"):
+
+	def __init__(self, planArm="left_arm"):
 		# Initialize the move_group API
 		moveit_commander.roscpp_initialize(sys.argv)
 
@@ -17,26 +18,19 @@ class Path_Planning:
 		rospy.init_node('moveit_circular_motion', anonymous=True)
 
 		# Connect to the arm move group
-		if self.arm == "right_arm":
+		if planArm == "right_arm":
 			self.arm = MoveGroupCommander('right_arm')
 		else:
 			self.arm = MoveGroupCommander('left_arm')
 
 		# Allow replanning to increase the odds of a solution
-		arm.allow_replanning(True)
+		self.arm.allow_replanning(True)
 
 		# Allow some leeway in position(meters) and orientation (radians)
-		arm.set_goal_position_tolerance(0.001)
-		arm.set_goal_orientation_tolerance(0.01)
+		self.arm.set_goal_position_tolerance(0.001)
+		self.arm.set_goal_orientation_tolerance(0.01)
 
 	def compute_cartesian_path(self, waypoints):
-		# Get the name of the end-effector link
-		end_effector_link = arm.get_end_effector_link()
-		# Plan and execute a trajectory to the goal configuration
-		self.arm.go()
-		rospy.sleep(1)
-		# Get the current pose so we can add it as a waypoint
-		start_pose = self.arm.get_current_pose(end_effector_link).pose
 
 		fraction = 0.0
 		maxtries = 100
@@ -47,12 +41,11 @@ class Path_Planning:
 
 		# Plan the Cartesian path connecting the waypoints
 		while fraction < 1.0 and attempts < maxtries:
-			(plan, fraction) = arm.compute_cartesian_path (
+			(plan, fraction) = self.arm.compute_cartesian_path (
 								 waypoints,   # waypoint poses
 								 0.01,        # eef_step
 								 0.0,         # jump_threshold
 								 False)       # avoid_collisions
-
 			# Increment the number of attempts
 			attempts += 1
 
@@ -63,45 +56,79 @@ class Path_Planning:
 		# If we have a complete plan, execute the trajectory
 		if fraction == 1.0:
 			rospy.loginfo("Path computed successfully. Moving the arm.")
-			arm.execute(plan)
+			self.arm.execute(plan)
 			rospy.loginfo("Path execution complete.")
 		else:
 			rospy.loginfo("Path planning failed with only " + str(fraction) + " success after " + str(maxtries) + " attempts.")
 
-	def circular_path(self):
+	def circular_path(self, cycles=2):
+		# Get the name of the end-effector link
+		end_effector_link = self.arm.get_end_effector_link()
+
+		# Get the current pose so we can add it as a waypoint
+		start_pose = self.arm.get_current_pose(end_effector_link).pose
+		print "start pose\n", start_pose
 		waypoints = []
-		#wpose = deepcopy(current_pose)
+		
 		wpose=Pose()
-
-		wpose.position.x = 0.619027463078
-		wpose.position.y = 0.456932535372
-		wpose.position.z = 0.35963384769
-
-		wpose.orientation.x = 0.89646727127
-		wpose.orientation.y = -0.441927661622
-		wpose.orientation.z = -0.0323472356087
-		wpose.orientation.w = 0.000172577279596
+		wpose.position.x = 0.741
+		wpose.position.y = 0.478
+		wpose.position.z = 0.153
+	
+		wpose.orientation.x = -0.4644
+		wpose.orientation.y = 0.8838
+		wpose.orientation.z = 0.0157
+		wpose.orientation.w = 0.0534
 
 		# Append the pose to the waypoints list
 		waypoints.append(deepcopy(wpose))
-		radius = 0.05
-		for a in range(0, 28, 1):
-			tempX = radius*math.cos(a*math.pi/4)
-			tempY = radius*math.sin(a*math.pi/4)
-			wpose.position.x += tempX
-			wpose.position.y += tempY
-			waypoints.append(deepcopy(wpose))
 
+		radius = 0.05
+		stepSize = math.pi/4
+
+		for i in range(cycles):
+			step = 0
+			while step*stepSize < 2*math.pi:
+				tempPose = deepcopy(wpose)
+				tempX = radius*math.cos(step*stepSize)
+				tempY = radius*math.sin(step*stepSize)
+				tempPose.position.x += tempX
+				tempPose.position.y += tempY
+				waypoints.append(tempPose)
+				step +=1
 		return waypoints
 
 if __name__ == "__main__":
-	cartesianPath = Path_Planning("left_arm")
-	wayspoints = cartesianPath.circular_path();
-	cartesianPath.compute_cartesian_path(wayspoints)
+	
+	# demo: do circular motion on left arm
 
-	# Move normally back to the 'neutral' position
-	arm.set_named_target('left_neutral')
-	arm.go()
+	cartesianPath = Path_Planning("left_arm")
+	
+	# define a starting pose
+	wpose=Pose()
+	wpose.position.x = 0.741
+	wpose.position.y = 0.478
+	wpose.position.z = 0.153
+	wpose.orientation.x = -0.4644
+	wpose.orientation.y = 0.8838
+	wpose.orientation.z = 0.0157
+	wpose.orientation.w = 0.0534
+
+	# move to the defined pose
+	cartesianPath.arm.set_pose_target(wpose)
+	cartesianPath.arm.go()
+	rospy.sleep(1)
+	
+	# get the circular path waypoints
+	wayspoints = cartesianPath.circular_path();
+
+	# move the arm following the wayspoints
+	cartesianPath.compute_cartesian_path(wayspoints)
+	rospy.sleep(1)
+
+	# Move back to the 'neutral' position (defined in SRDF file)
+	cartesianPath.arm.set_named_target('left_neutral')
+	cartesianPath.arm.go()
 	rospy.sleep(1)
 
 	# Shut down MoveIt cleanly
